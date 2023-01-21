a15e87f0e02238e0ac17702610ed5b2e5d241c73
kenny-skaggs <skaggskd88@gmail.com>
2023-01-21 12:10:16 -0800

    setting up webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    cache: {
+        type: 'filesystem',
+    },
+    entry: path.join(__dirname, 'src', 'app.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: path.join(__dirname, 'src', 'index.html'),
+        })
+    ],
+    module: {
+        rules:[
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
