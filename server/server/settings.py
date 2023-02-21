d0c401cd97304096fcca2d401e0f94da6cd1e6d0
kenny-skaggs <skaggskd88@gmail.com>
2023-02-20 16:50:49 -0800

    camelCasing rest framework response

--- server/server/settings.py
+++ server/server/settings.py
@@ -147,7 +147,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+    )
 }
