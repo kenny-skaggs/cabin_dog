9a7a3d883dcd452301f965ff10a81c942e0a75c3
kenny-skaggs <skaggskd88@gmail.com>
2023-02-24 22:50:24 -0800

    getting started with visx

--- client/src/features/calculation/calculationDetails.js
+++ client/src/features/calculation/calculationDetails.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Group } from '@visx/group';
+import { Bar } from '@visx/shape';
+import { scaleLinear, scaleBand, scaleOrdinal } from '@visx/scale';
 
 import Button from '../../components/Button';
 import CalculationSummary from './calculationSummary';
@@ -8,10 +11,77 @@ import Modal from '../../components/ContentModal';
 
 import { closeCalculationModal, postPayment } from './calculationSlice';
 
+
 class CalculationDetails extends Component {
     render () {
+        const personData = this.props.personList.map((person) => {
+            const percentResponsible = person.availableIncome / this.props.totalIncome;
+            const amountResponsible = percentResponsible * this.props.totalPaid;
+            const amountOverpaid = person.paid - amountResponsible;
+            return {
+                paid: person.paid,
+                responsible: amountResponsible,
+                overPaid: amountOverpaid,
+                name: person.name,
+                id: person.id
+            }
+        });
+
+        console.log(personData);
+        
+        // Define the graph dimensions and margins
+        const width = 500;
+        const height = 100;
+        const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+        
+        // Then we'll create some bounds
+        const xMax = width - margin.left - margin.right;
+        const yMax = height - margin.top - margin.bottom;
+        
+        // We'll make some helpers to get at the data we want
+        const x = d => d.paid;
+        const y = d => d.id;
+        
+        // And then scale the graph by our data
+        const xScale = scaleLinear({
+            range: [0, xMax],
+            round: true,
+            domain: [0, this.props.totalPaid],
+          });
+        const yScale = scaleBand({
+          range: [0, yMax],
+          round: true,
+          domain: personData.map(y)
+        });
+        const colorScale = scaleOrdinal({
+            domain: personData.map(y),
+            range: ['blue', 'green']
+        });
+        
+        // Compose together the scale and accessor functions to get point functions
+        const compose = (scale, accessor) => personData => scale(accessor(personData));
+        const xPoint = compose(xScale, x);
+        const yPoint = compose(yScale, y);
+
         return (
             <Modal showModal={this.props.showModal}>
+                <svg width={width} height={height}>
+                    {personData.map((d, i) => {
+                        const barWidth = xMax - xPoint(d);
+                        return (
+                            <Group key={`bar-${i}`}>
+                                <Bar
+                                    x={0}
+                                    y={yPoint(d)}
+                                    height={yScale.bandwidth()}
+                                    width={barWidth}
+                                    fill={colorScale(d.id)}
+                                />
+                            </Group>
+                        );
+                    })}
+                    </svg>
+
                 <div className='calculation-view'>
                     <table className='table is-bordered is-fullwidth'>
                         <thead>
@@ -43,7 +113,6 @@ class CalculationDetails extends Component {
                                 <td><CurrencyDisplay amount={this.props.totalIncome} /></td>
                                 <td />
                                 <td><CurrencyDisplay amount={this.props.totalPaid} /></td>
-                                {/* <td><CurrencyDisplay amount={Object.values(userResponsible).reduce((partialSum, amount) => amount + partialSum, 0)} /></td> */}
                             </tr>
                         </tbody>
                     </table>
@@ -51,7 +120,6 @@ class CalculationDetails extends Component {
                 <br />
                 <CalculationSummary />
                 <div className='buttons is-pulled-right'>
-                    {/* <div className='calculation-result'>{result}</div> */}
                     <Button onClick={() => this.props.postPayment(this.props.balanceAmount)}>Paid</Button>
                     <Button onClick={() => this.props.closeModal()}>Cancel</Button>
                 </div>
